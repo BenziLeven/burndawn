152e35c32452845103a0e85d5a49a5018a7ea5d2
jeremybenzakein <jeremy.benzakein@360learning.com>
2021-05-05 17:29:54 +0200

    lint and update title

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        burndawn
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <div>
+            <Logo />
+            <h1 class="title">
+                BurnDawn
+            </h1>
+            <div class="links">
+                <a
+                    href="https://nuxtjs.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="button--green"
+                >
+                    Documentation
+                </a>
+                <a
+                    href="https://github.com/nuxt/nuxt.js"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="button--grey"
+                >
+                    GitHub
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
 
 <style lang="scss" scoped>
