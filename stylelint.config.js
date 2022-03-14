a6be0ff9bc38e8f1e08433cd97c49661f63a8933
jeremybenzakein <jeremy.benzakein@360learning.com>
2022-03-14 18:22:34 +0100

    setup nuxt project

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    extends: [
-        "stylelint-config-standard"
-    ],
-    // add your custom config here
-    // https://stylelint.io/user-guide/configuration
-    rules: {
-        indentation: 4
-    }
-};
+  customSyntax: 'postcss-html',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue'
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {}
+}
