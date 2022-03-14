79555387114271bc19f06edc16cefcbf90a24190
jeremybenzakein <jeremy.benzakein@360learning.com>
2022-03-14 19:15:46 +0100

    setup basis for layout

---
+++ components/navigation/SidePanel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="side-panel">
+    side panel
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidePanel"
+}
+</script>
+
+<style scoped lang="scss">
+.side-panel {
+  position: absolute;
+  left: 0;
+
+  height: 100%;
+  width: $side-panel-width-collapsed;
+
+  transition: width .3s ease;
+
+  background: #919467;
+
+&:hover {
+   width: $side-panel-width-open;
+ }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="default-layout">
-    default layout
+    <SidePanel class="side-panel" />
+    <div class="page-body">
+      <nuxt />
+    </div>
   </div>
 </template>
 
 <script>
+import SidePanel from "@/components/navigation/SidePanel"
 export default {
-  name: "DefaultLayout"
+  name: "DefaultLayout",
+  components: {
+    SidePanel
+  }
 }
 </script>
 
 <style scoped lang="scss">
+
 .default-layout {
   width: 100%;
   height: 100%;
 
   min-height: 100vh;
+  padding-left: $side-panel-width-collapsed;
 
   background-color: #00716f;
 
